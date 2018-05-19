3ca6d4aebaf476d992f85e33abc66d8e37703c81
Enigma James <enigma_1m2@yahoo.com>
2018-05-19 17:19:27 -0400

    create and read for java config file instead of xml configuration

---
+++ src/com/love2code/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.love2code.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.love2code.springdemo")
+public class SportConfig {
+
+}
