0aaf5da58a23e856e6b5a25576a2845390d0c840
Enigma James <enigma_1m2@yahoo.com>
2018-05-19 17:06:29 -0400

    add @PostConstruct and @PreDestroy methods -- bean lifecycle

--- src/com/love2code/springdemo/TennisCoach.java
+++ src/com/love2code/springdemo/TennisCoach.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class TennisCoach implements Coach{
 
@@ -24,5 +27,17 @@ public class TennisCoach implements Coach{
     @Override
     public String getDailyFortune(){
         return fortuneService.getFortune();
+    }
+
+    // define my init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println(">> TennisCoach: inside do myStartupStuff() <<");
+    }
+
+    // define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println(">> TennisCoach: inside do doMyCleanupStuff() <<");
     }
 }
