6e056042a9d42b540058d407416e4e493b3bff7b
Enigma James <enigma_1m2@yahoo.com>
2018-05-19 17:35:18 -0400

    create beans in java config file for a new swimclass

---
+++ src/com/love2code/springdemo/SadFortuneService.java
@@ -0,0 +1,8 @@
+package com.love2code.springdemo;
+
+public class SadFortuneService implements FortuneService {
+    @Override
+    public String getFortune() {
+        return "Today is a sad day";
+    }
+}
--- src/com/love2code/springdemo/SportConfig.java
+++ src/com/love2code/springdemo/SportConfig.java
@@ -1,10 +1,24 @@
 package com.love2code.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.love2code.springdemo")
+//@ComponentScan("com.love2code.springdemo")
 public class SportConfig {
+
+    // define bean for our sad fortune service
+    @Bean
+    public FortuneService sadFortuneService(){
+        return new SadFortuneService();
+    }
+
+    // define bean for our swim coach and inject dependency
+    @Bean
+    public Coach swimCoach(){
+        SwimCoach theCoach = new SwimCoach(sadFortuneService());
+        return theCoach;
+    }
 
 }
