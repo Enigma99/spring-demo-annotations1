2a0276dcebdc76af6d1e762545bd5e0c94ca1e71
Enigma James <enigma_1m2@yahoo.com>
2018-05-19 16:40:48 -0400

    add implementation for RandomFortuneService and inject in the app

--- src/com/love2code/springdemo/RandomFortuneService.java
+++ src/com/love2code/springdemo/RandomFortuneService.java
@@ -2,11 +2,29 @@ package com.love2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RandomFortuneService implements FortuneService {
 
+    // create an array of strings
+    private String[] fortunes = {
+            "Beware of the wolf in sheep's clothing",
+            "Diligence is the mother of good luck",
+            "The Journey is the reward"
+    };
+
+    // create a random number generator
+    private Random random = new Random();
+
     @Override
     public String getFortune() {
-        return null;
+
+        // pick a random fortune out of the array
+        int index = random.nextInt(fortunes.length);
+
+        String fortune = fortunes[index];
+
+        return fortune;
     }
 }
