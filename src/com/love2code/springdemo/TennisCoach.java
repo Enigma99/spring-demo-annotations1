e3f97a44d0946c1ac4f3a25ae8c21a33ae96b25d
Enigma James <enigma_1m2@yahoo.com>
2018-05-19 16:03:43 -0400

    add dependency injection using setter inject via autowiring

--- src/com/love2code/springdemo/TennisCoach.java
+++ src/com/love2code/springdemo/TennisCoach.java
@@ -8,9 +8,16 @@ public class TennisCoach implements Coach{
 
     private FortuneService fortuneService;
 
-    @Autowired
-    public TennisCoach(FortuneService fortuneService) {
-        this.fortuneService = fortuneService;
+//    @Autowired
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
+
+    // define a defualt constructor
+
+
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
     }
 
     @Override
@@ -21,5 +28,12 @@ public class TennisCoach implements Coach{
     @Override
     public String getDailyFortune(){
         return fortuneService.getFortune();
+    }
+
+    // define a setter method for dependency injection
+    @Autowired
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside setFortuneService()");
+        this.fortuneService = fortuneService;
     }
 }
